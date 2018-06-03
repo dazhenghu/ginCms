5b395483fdbc94724bc72f2e9a0e68356b364812
huhao <huhao@qianshengqian.com>
2018-06-04 02:06:00 +0800

    ueditor接口完善

--- admin/controller/ueditor.go
+++ admin/controller/ueditor.go
@@ -36,13 +36,26 @@ func (ued *ueditorController) index(context *gin.Context) {
 
     switch action {
     case "config":
+        // config接口
         ued.config(context)
     case "uploadimage":
+        // 上传图片
         ued.uploadImage(context)
     case "uploadscrawl":
+        // 上传涂鸦
         ued.uploadScrawl(context)
     case "uploadvideo":
+        // 上传视频
         ued.uploadVideo(context)
+    case "uploadfile":
+        // 上传附件
+        ued.uploadfile(context)
+    case "listfile":
+        // 查询上传的文件列表
+        ued.listFile(context)
+    case "listimage":
+        // 查询上传的图片列表
+        ued.listImage(context)
     }
 
 }
@@ -65,4 +78,21 @@ func (ued *ueditorController) uploadScrawl(context *gin.Context)  {
 func (ued *ueditorController) uploadVideo(context *gin.Context)  {
     res, _ := uedService.UploadVideo(context.Request)
     context.JSON(http.StatusOK, res)
+}
+
+func (ued *ueditorController) uploadfile(context *gin.Context)  {
+    res, _ := uedService.UploadFile(context.Request)
+    context.JSON(http.StatusOK, res)
+}
+
+func (ued *ueditorController) listFile(context *gin.Context) {
+    listFileItem := &gueditor.ListFileItem{}
+    uedService.Listfile(listFileItem, 0, 10)
+    context.JSON(http.StatusOK, listFileItem)
+}
+
+func (ued *ueditorController) listImage(context *gin.Context)  {
+    listFileItem := &gueditor.ListFileItem{}
+    uedService.ListImage(listFileItem, 0, 10)
+    context.JSON(http.StatusOK, listFileItem)
 }
