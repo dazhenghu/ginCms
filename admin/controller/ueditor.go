a31197870f0e8a602185931692c3ce02a86e971d
huhao <huhao@qianshengqian.com>
2018-06-03 22:38:02 +0800

    ueditor解决生成文件夹权限不对问题，以及自定义ueditor配置

--- admin/controller/ueditor.go
+++ admin/controller/ueditor.go
@@ -6,7 +6,8 @@ import (
     "github.com/dazhenghu/gueditor"
     "github.com/dazhenghu/util/fileutil"
     "net/http"
-    "fmt"
+    "path/filepath"
+    "syscall"
 )
 
 type ueditorController struct {
@@ -20,8 +21,11 @@ func init()  {
     ueditorInstance = &ueditorController{}
     ueditorInstance.Init(ueditorInstance)
 
+    syscall.Umask(0)
     rootPath, _ := fileutil.GetCurrentDirectory()
-    uedService, _ = gueditor.NewService(nil, nil, rootPath, "")
+    configFilePath := filepath.Join(rootPath, "config/ueditor.json") // 设置自定义配置文件路径
+    rootPath = filepath.Join(rootPath, "../") // 设置项目根目录
+    uedService, _ = gueditor.NewService(nil, nil, rootPath, configFilePath)
 
     ueditorInstance.PostAndGet("/ueditor", ueditorInstance.index)
 }
@@ -29,8 +33,11 @@ func init()  {
 func (ued *ueditorController) index(context *gin.Context) {
     action := context.Query("action")
 
-    if action == "config" {
+    switch action {
+    case "config":
         ued.config(context)
+    case "uploadimage":
+        ued.uploadImage(context)
     }
 
 }
@@ -38,4 +45,9 @@ func (ued *ueditorController) index(context *gin.Context) {
 func (ued *ueditorController) config(context *gin.Context) {
     cnf := uedService.Config()
     context.JSON(http.StatusOK, cnf)
+}
+
+func (ued *ueditorController) uploadImage(context *gin.Context)  {
+    res, _ := uedService.Uploadimage(context.Request)
+    context.JSON(http.StatusOK, res)
 }
