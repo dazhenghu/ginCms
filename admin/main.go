45457157a0a269ed3f5117f0d382d65e1e94b584
huhao <huhao@qianshengqian.com>
2018-06-04 11:50:44 +0800

    APP生成放在外层main中，避免产生循环依赖

--- admin/main.go
+++ admin/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "github.com/dazhenghu/ginCms/admin/web"
+import (
+    "github.com/dazhenghu/ginApp"
+    "github.com/dazhenghu/util/fileutil"
+    "path"
+    "path/filepath"
+    "github.com/dazhenghu/ginCms/admin/web"
+    "github.com/dazhenghu/ginCms/common/service"
+    _ "github.com/dazhenghu/ginCms/admin/controller"
+)
 
 func main()  {
-    web.Run()
+    Run()
+}
+
+func Run()  {
+    ginAppInstance := ginApp.Instance()
+    // 保存全局app实例
+    web.App = ginAppInstance
+    // 获取挡墙文件夹
+    currPath, _ := fileutil.GetCurrentDirectory()
+    // 设置common文件夹位置
+    web.App.SetCommonPath(path.Join(currPath, "../common"))
+    // 设置根目录文件夹位置
+    web.App.SetRootPath(currPath)
+    // 读取默认位置的配置文件
+    web.App.DefaultLoadConfig("")
+    // static参数：relativePath，表示模板中的设定的读取资源文件的路径;root，表示真实的静态文件物理路径。
+    // 相当于url中relativePath映射到root
+    web.App.Engine().Static(web.App.AppConfig.Pulic.RelativePath, web.App.AppConfig.Pulic.Root)
+    // 加载html
+    htmlDir := filepath.Join(currPath, web.App.AppConfig.ViewBaseDir)
+    web.App.Engine().LoadHTMLGlob(htmlDir)
+
+    // service层初始化
+    service.Init(web.App.AppConfig.Dblist)
+
+    addr := web.App.AppConfig.Addr
+    if addr == "" {
+        addr = ":8001"
+    }
+    web.App.Run(web.App.AppConfig.Addr)
 }
--- admin/web/index.go
+++ admin/web/index.go
@@ -2,38 +2,6 @@ package web
 
 import (
     "github.com/dazhenghu/ginApp"
-    _ "github.com/dazhenghu/ginCms/admin/controller"
-    "path/filepath"
-    "github.com/dazhenghu/util/fileutil"
-    "path"
-    "github.com/dazhenghu/ginCms/common/service"
 )
 
 var App *ginApp.GinApp
-
-func Run()  {
-    App = ginApp.Instance()
-    // 获取挡墙文件夹
-    currPath, _ := fileutil.GetCurrentDirectory()
-    // 设置common文件夹位置
-    App.SetCommonPath(path.Join(currPath, "../common"))
-    // 设置根目录文件夹位置
-    App.SetRootPath(currPath)
-    // 读取默认位置的配置文件
-    App.DefaultLoadConfig("")
-    // static参数：relativePath，表示模板中的设定的读取资源文件的路径;root，表示真实的静态文件物理路径。
-    // 相当于url中relativePath映射到root
-    App.Engine().Static(App.AppConfig.Pulic.RelativePath, App.AppConfig.Pulic.Root)
-    // 加载html
-    htmlDir := filepath.Join(currPath, App.AppConfig.ViewBaseDir)
-    App.Engine().LoadHTMLGlob(htmlDir)
-
-    // service层初始化
-    service.Init(App.AppConfig.Dblist)
-
-    addr := App.AppConfig.Addr
-    if addr == "" {
-        addr = ":8001"
-    }
-    App.Run(App.AppConfig.Addr)
-}
